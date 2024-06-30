167e96df4bdebcd63a1aa4647b3962cf2872233c
pavan <balubadi.pavankumar@gmail.com>
2024-06-30 17:25:55 +0530

    expense-terraform

--- main.tf
+++ main.tf
@@ -133,16 +133,3 @@ resource "aws_nat_gateway" "ngw" {
 
 }
 
-resource "aws_vpc_peering_connection" "peer" {
-  peer_owner_id = var.account_id
-  peer_vpc_id   = var.default_vpc_id
-  vpc_id        = aws_vpc.main.id
-  auto_accept   = true
-  tags          = merge(var.tags, { Name = "peer-for-${var.env}-vpc-to-default-vpc" })
-}
-
-resource "aws_route" "default-vpc-peer-route" {
-  route_table_id            = var.default_route_table_id
-  destination_cidr_block    = vpc_cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
